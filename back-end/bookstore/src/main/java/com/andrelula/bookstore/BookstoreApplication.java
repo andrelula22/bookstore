b956f4d69ad7d3efd9375a7acece23700833c471
andrelula22 <andrelula78@gmail.com>
2022-07-08 12:21:50 -0300

    instancia automatica base de dados

--- back-end/bookstore/src/main/java/com/andrelula/bookstore/BookstoreApplication.java
+++ back-end/bookstore/src/main/java/com/andrelula/bookstore/BookstoreApplication.java
@@ -1,13 +1,38 @@
 package com.andrelula.bookstore;
 
+import com.andrelula.bookstore.domain.Categoria;
+import com.andrelula.bookstore.domain.Livro;
+import com.andrelula.bookstore.repositories.CategoriaRepository;
+import com.andrelula.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica", "livros de TI");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+
+	}
 }
